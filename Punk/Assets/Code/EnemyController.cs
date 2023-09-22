348cf6c97caa4266570761ec244f4bab3478dd88
JKoonce2002 <71308122+JKoonce2002@users.noreply.github.com>
2023-09-22 15:35:23 -0500

    Added Enemy Animation
    Just walking and idle for now

--- Punk/Assets/Code/EnemyController.cs
+++ Punk/Assets/Code/EnemyController.cs
@@ -9,41 +9,45 @@ namespace Punk
         //Outlet
         Rigidbody2D _rb;
         SpriteRenderer sprite;
+        Animator animator;
 
         //State Tracking
-        bool directionRight;
+        bool directionLeft;
 
         // Start is called before the first frame update
         void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
             sprite = GetComponent<SpriteRenderer>();
-            directionRight = true;
+            animator = GetComponent<Animator>();
+            directionLeft = true;
         }
 
         // Update is called once per frame
         void Update()
         {
-            float defaultSpeed = 10f;
+            float defaultSpeed = 12f;
 
-            if (directionRight)
+            if (directionLeft)
+            {
+                _rb.AddForce(Vector2.left * defaultSpeed * Time.deltaTime, ForceMode2D.Impulse);
+                sprite.flipX = true;
+            }
+
+            else
             {
                 _rb.AddForce(Vector2.right * defaultSpeed * Time.deltaTime, ForceMode2D.Impulse);
                 sprite.flipX = false;
             }
 
-            else
-            {
-                _rb.AddForce(Vector2.left * defaultSpeed * Time.deltaTime, ForceMode2D.Impulse);
-                sprite.flipX = true;
-            }
+            animator.SetFloat("Speed", _rb.velocity.magnitude);
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (!other.gameObject.GetComponent<PlayerController>())
             {
-                directionRight = !directionRight;
+                directionLeft = !directionLeft;
             }
         }
     }
