0c7991f986dd82ce33283a3eb6baf56f2556292e
Luke Hermann <hermannl@wustl.edu>
2023-11-17 14:05:01 -0600

    Allowed duplicate rebinds

--- Punk/Assets/Samples/Input System/1.6.3/Rebinding UI/RebindActionUI.cs
+++ Punk/Assets/Samples/Input System/1.6.3/Rebinding UI/RebindActionUI.cs
@@ -267,13 +267,6 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
 
             // Configure the rebind.
             m_RebindOperation = action.PerformInteractiveRebinding(bindingIndex)
-                .WithControlsExcluding("<Mouse>/leftButton")
-                .WithControlsExcluding("<Mouse>/rightButton")
-                .WithControlsExcluding("<Mouse>/press")
-                .WithControlsExcluding("<Keyboard>/space")
-                .WithControlsExcluding("<Keyboard>/a")
-                .WithControlsExcluding("<Keyboard>/d")
-                .WithControlsExcluding("<Keyboard>/leftShift")
                 .WithCancelingThrough("<Keyboard>/escape")
                 .OnCancel(
                     operation =>
